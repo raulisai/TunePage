1f01f61965b8dd2181cc2d654c93faf268220131
IsaiMonroy <raulisai97@gmail.com>
2022-03-30 04:17:22 -0600

    Se terminaron los trastes de la guitarra

--- src/components/TrastesInstrument/Trastes.js
+++ src/components/TrastesInstrument/Trastes.js
@@ -1,94 +1,122 @@
 /* eslint-disable no-useless-constructor */
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React from "react";
 
-import "./css/trastes.css"
 
-//components
-import fret from "./fret.js"
+import "./css/trastes.css";
+
 
 
 //import {GetWaveInHz} from '../../helpers/redux/reducers/actionTuner/GetWaveInHz'
 
- function Trastes(){ 
-    //constantes
-const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21]
-const doubleFretMarkPositions = [12, 24]
-const notesFlat = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
-const notesSharp = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
-const instrumentTuningPresets = {
-          'Guitar': [4, 11, 7, 2, 9, 4],
-          'Bass (4 strings)': [7, 2, 9, 4],
-          'Bass (5 strings)': [7, 2, 9, 4, 11],
-          'Ukulele': [9, 4, 0, 7]
-      }
+function Trastes(props) {
+  //constantes
+  const singleFretMarkPositions = [3, 5, 7, 9, 15, 17, 19, 21];
+  const doubleFretMarkPositions = [12, 24];
+  const notesFlat = [
+    "C",
+    "Db",
+    "D",
+    "Eb",
+    "E",
+    "F",
+    "Gb",
+    "G",
+    "Ab",
+    "A",
+    "Bb",
+    "B",
+  ];
+  const notesSharp = [
+    "C",
+    "C#",
+    "D",
+    "D#",
+    "E",
+    "F",
+    "F#",
+    "G",
+    "G#",
+    "A",
+    "A#",
+    "B",
+  ];
+  const instrumentTuningPresets = {
+    Guitar: [4, 11, 7, 2, 9, 4],
+    "Bass (4 strings)": [7, 2, 9, 4],
+    "Bass (5 strings)": [7, 2, 9, 4, 11],
+    Ukulele: [9, 4, 0, 7],
+  };
 
-let allNotes;
-let showMultipleNotes = false;
-let showAllNotes = false;
-let numberOfFrets = 19;
-let accidentals = 'flats';
-let selectedInstrument = 'Guitar';
-let numberOfStrings = instrumentTuningPresets[selectedInstrument].length;
+  let numberOfFrets = 19;
+  let accidentals = "sharps";
+  let selectedInstrument = "Guitar";
+  let numberOfStrings = instrumentTuningPresets[selectedInstrument].length;
 
-let frets = []
-let noteFret=[]
+  let frets = [];
+  let noteFret = [];
 
-
-function generateNoteNames(noteIndex, accidentals){
+  function generateNoteNames(noteIndex, accidentals) {
     noteIndex = noteIndex % 12;
     let noteName;
-    if (accidentals === 'flats') {
-        noteName = notesFlat[noteIndex];
-    } else if (accidentals === 'sharps') {
-        noteName = notesSharp[noteIndex];
+    if (accidentals === "flats") {
+      noteName = notesFlat[noteIndex];
+    } else if (accidentals === "sharps") {
+      noteName = notesSharp[noteIndex];
     }
     return noteName;
-}
+  }
 
-for (let i = 0; i < numberOfStrings; i++){
-    noteFret=[]
-   
-        for ( let fret = 0; fret <= numberOfFrets; fret++) {
-            let noteName = generateNoteNames((fret + instrumentTuningPresets[selectedInstrument][i]), accidentals);
-                console.log(noteName)
-                //noteFret.setAttribute('data-note', noteName);
-            if(i === 0 && singleFretMarkPositions.indexOf(fret) !== -1){
-                noteFret.push(<div className="note-fret single-fretmark">{ 
-                    noteName
-                     }</div>)
-            }
-            else if(i === 0  && doubleFretMarkPositions.indexOf(fret) !== -1){
-                noteFret.push(<div className="note-fret single-fretmark">{ 
-                    noteName
-                     }</div>)
-            }
-            else{
-            noteFret.push(<div className="note-fret">{ 
-           noteName
-            }</div>)
-        }
-        } 
+
+  for (let i = 0; i < numberOfStrings; i++) {
+    let note=props.notaMusic != undefined ? props.notaMusic : " "
+    let noteclean= note.toString().substr(0, note.length - 1)
+    noteFret = [];
+
+    for (let fret = 0; fret <= numberOfFrets; fret++) {
+      let noteName = generateNoteNames(
+        fret + instrumentTuningPresets[selectedInstrument][i],
+        accidentals
+      );
     
-    
-    frets.push(<div className="string">{ 
-        noteFret
-    }</div>)
-}
-
-
-  
-        return (
-            <>
-            <div className="fretboard">
-            {frets}
+      if (i === 0 && singleFretMarkPositions.indexOf(fret) !== -1) {
+        
+        noteFret.push(
+          <div className="note-fret single-fretmark">
+            <div className={ noteclean === noteName ? "note-simb-active":"note-simb-hiden"}>
+              {
+                noteName 
+              }
             </div>
-            <div className="note-name-section">
-                
+          </div>
+        );
+      } else if (i === 0 && doubleFretMarkPositions.indexOf(fret) !== -1) {
+        
+        noteFret.push(
+          <div className="note-fret single-fretmark">
+            <div className={ noteclean === noteName ? "note-simb-active":"note-simb-hiden"}>
+              {noteName}
             </div>
-            </>
-        )
-    
+          </div>
+        );
+      } else {
+        noteFret.push(
+          <div className="note-fret">
+            <div className={ noteclean === noteName ? "note-simb-active":"note-simb-hiden"}>
+              {noteName}
+            </div>
+          </div>
+        );
+      }
+    }
+
+    frets.push(<div className="string">{noteFret}</div>);
+  }
+
+  return (
+    <>
+      <div className="fretboard">{frets}</div>
+      <div className="note-name-section"></div>
+    </>
+  );
 }
 export default Trastes;
-
--- src/pages/Tuner.js
+++ src/pages/Tuner.js
@@ -14,8 +14,6 @@ import store from "../helpers/redux/store";
 export default class Home extends Component {
   constructor(props) {
     super(props);
-    console.log("1. constructor()");
-
     this.state = {
       data: [],
       Activo: false,
@@ -202,93 +200,93 @@ export default class Home extends Component {
 
       let noteArray = [
         "C1",
-        "C1#",
+        "C#1",
         "D1",
-        "D1#",
+        "D#1",
         "E1",
         "F1",
-        "F1#",
+        "F#1",
         "G1",
-        "G1#",
+        "G#1",
         "A1",
-        "A1#",
+        "A#1",
         "B1",
         "C2",
-        "C2#",
+        "C#2",
         "D2",
-        "D2#",
+        "D#2",
         "E2",
         "F2",
-        "F2#",
+        "F#2",
         "G2",
-        "G2#",
+        "G#2",
         "A2",
-        "A2#",
+        "A#2",
         "B2",
         "C3",
-        "C3#",
+        "C#3",
         "D3",
-        "D3#",
+        "D#3",
         "E3",
         "F3",
-        "F3#",
+        "F#3",
         "G3",
-        "G3#",
+        "G#3",
         "A3",
-        "A3#",
+        "A#3",
         "B3",
         "C4",
-        "C4#",
+        "C#4",
         "D4",
-        "D4#",
+        "D#4",
         "E4",
         "F4",
-        "F4#",
+        "F#4",
         "G4",
-        "G4#",
+        "G#4",
         "A4",
-        "A4#",
+        "A#4",
         "B4",
         "C5",
-        "C5#",
+        "C#5",
         "D5",
-        "D5#",
+        "D#5",
         "E5",
         "F5",
-        "F5#",
+        "F#5",
         "G5",
-        "G5#",
+        "G#5",
         "A5",
-        "A5#",
+        "A#5",
         "B5",
         "C6",
-        "C6#",
+        "C#6",
         "D6",
-        "D6#",
+        "D#6",
         "E6",
         "F6",
-        "F6#",
+        "F#6",
         "G6",
-        "G6#",
+        "G#6",
         "A6",
-        "A6#",
+        "A#6",
         "B6",
         "C7",
-        "C7#",
+        "C#7",
         "D7",
-        "D7#",
+        "D#7",
         "E7",
         "F7",
-        "F7#",
+        "F#7",
         "G7",
-        "G7#",
+        "G#7",
         "A7",
-        "A7#",
+        "A#7",
         "B7",
       ];
 
       // Nodo de audio.
-      console.log(contextoDeAudio);
+      //console.log(contextoDeAudio);
       let analizador = contextoDeAudio.createAnalyser();
       let bitCounter = contextoDeAudio.sampleRate;
       datos_de_fuente.connect(analizador);
@@ -344,7 +342,7 @@ export default class Home extends Component {
         this.setState({ Nota: noteArray[noteMusic]  })
        // console.log("hz --> " + Hz);
         //console.log("wave ---> " + wave);
-        console.log("Nota: " + noteArray[noteMusic]);
+        //console.log("Nota: " + noteArray[noteMusic]);
         if(this.state.Activo === false) 
         {
             clearInterval(InicializarInterval);
@@ -422,7 +420,7 @@ export default class Home extends Component {
               notaMusic= {this.state.Nota}/>
             </div>
             <div className="col-11">
-              <Trastes />
+              <Trastes notaMusic= {this.state.Nota}/>
             </div>
           </div>
         </div>
