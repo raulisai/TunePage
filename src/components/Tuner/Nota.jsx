a305c237b14bdc830c5fc333fa9b0c02d76b7a59
IsaiMonroy <raulisai97@gmail.com>
2022-04-23 15:59:19 -0500

    Se termino el css de tuner

--- src/components/Tuner/Nota.jsx
+++ src/components/Tuner/Nota.jsx
@@ -16,11 +16,19 @@ class PrintNote extends React.Component {
       <div className="MainBoxTuner">
       <div className="NoteInLetters m-5">
       <h5 className="Hz">122<span className="TetxHz">Hz</span></h5>
-      <h5 className="NoteLeft">D#1</h5>
-      <h4 className="Note SetingMainNote">C#1</h4>
-      <h5 className="NoteRight">C#2</h5>
-      <div className="MarcoLeft"></div>
-      <div className="MarcoRight"></div>
+      <h5 className="NoteLeft">DO#</h5>
+      <h4 className="Note SetingMainNote">RE<span className="TetxOctv">4</span></h4>
+      <h5 className="NoteRight">RE#</h5>
+      <div className="MarcoLeft">
+      <div className="MarcoLeftInternal"></div>
+      </div>
+      <div className="MarcoRight">
+      <div className="MarcoRightInternal"></div></div>
+      <div className="lineCentral"></div>
+      <div className="line1"></div>
+      <div className="line2"></div>
+      <div className="line3"></div>
+      <div className="line4"></div>
       </div>
       </div>
 
