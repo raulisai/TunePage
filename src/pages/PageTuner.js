9fa652ffb1c6ca62c074e3a237effd4ef4c1428d
IsaiMonroy <raulisai97@gmail.com>
2022-05-08 20:45:40 -0500

    se sacaron funciones de la onda de la notas

--- src/pages/PageTuner.js
+++ src/pages/PageTuner.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import notasArray from "../helpers/objects/noteArray";
-import BorderNotas from "../helpers/objects/noteBorders";
-
+//style
 import "./styles/Home.css";
-
+//components
 import Tuner from "../components/Tuner/Nota";
 import Trastes from "../components/TrastesInstrument/Trastes";
-
 //React Redux
 import { Provider } from "react-redux";
 import store from "../helpers/redux/store";
+//functions
+import findWaveLength from "../helpers/services/FindWaveLength";
+import findNote from "../helpers/services/FindNote";
 
-export default class PTuner extends Component {
+export default class PageTuner extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -30,98 +31,6 @@ export default class PTuner extends Component {
   start = () => {
     this.setState({ Activo: true });
 
-    const findWaveLength = (e, r, t, o, n, a, i) => {
-      let s = [];
-
-      for (var u = 0, c = 0, l = 0, m = 0; m < e.length - 1; m++) {
-        s.push(e[m]);
-        for (var d = 1; i > d; d++) s.push(e[m] + ((e[m + 1] - e[m]) * d) / i);
-      }
-      s.push(e[e.length - 1]);
-      r *= i;
-      t *= i;
-      for (var g = s.length, f = [], h = 0, w = 0, A = 0; t > d; d++) {
-        if (Math.abs(s[d]) > h) {
-          w = d;
-          h = Math.abs(s[d]);
-        }
-        A += Math.abs(s[d]);
-      }
-      if (a > A / t) return -1;
-      if (0 === w || w === t) return -1;
-      for (
-        var C = 0,
-          D = 0,
-          v = 0,
-          y = 0,
-          p = 0,
-          T = 0,
-          F = 0,
-          b = 0,
-          k = 0,
-          d = r;
-        t >= d;
-        d++
-      ) {
-        F = 0;
-        b = 0;
-        C = 0;
-        D = 0;
-        for (var N = w; g > N; N += d) {
-          F += s[N];
-          if (0 !== b && g - 5 * i > N) {
-            k = s[N] / s[w];
-            if (k > 0) {
-              if (k > 1) k = 1;
-            }
-          }
-          u = s[N];
-          l = s[N - 5 * i];
-          c = s[N + 5 * i];
-
-          if (s[w] >= 0) {
-            if (u > c && u > l) {
-              C += (k * k * k * k * s[w] * o * (t - d)) / t;
-              D++;
-            } else if (c > u && l > u) {
-              C += (k * k * k * k * s[w] * o * (t - d)) / t;
-              D++;
-            }
-          }
-          b++;
-          b >= n && (N = g);
-        }
-        F += (C * D) / b;
-        F /= b;
-        if (F > v) {
-          v = F;
-          p = d;
-        } else if (y > F) {
-          y = F;
-          T = d;
-          f.push(F);
-        }
-      }
-      return s[w] >= 0 ? p / i : T / i;
-    };
-
-    function findNote(e) {
-      if (254.285 >= e) {
-        if (89.903 >= e) {
-          for (var r = 0; 17 >= r; r++)
-            if (e > BorderNotas[r][0] && e <= BorderNotas[r][1]) return r;
-        } else
-          for (r = 18; 35 >= r; r++)
-            if (e > BorderNotas[r][0] && e <= BorderNotas[r][1]) return r;
-      } else if (719.225 >= e) {
-        for (r = 36; 53 >= r; r++)
-          if (e > BorderNotas[r][0] && e <= BorderNotas[r][1]) return r;
-      } else
-        for (r = 54; 88 >= r; r++)
-          if (e > BorderNotas[r][0] && e <= BorderNotas[r][1]) return r;
-      return -1;
-    }
-
     const GetWaveInHz = (fuente) => {
       let contextoDeAudio = new (window.AudioContext ||
         window.webkitAudioContext ||
@@ -130,8 +39,6 @@ export default class PTuner extends Component {
         window.msAudioContext)();
       let datos_de_fuente = contextoDeAudio.createMediaStreamSource(fuente);
 
-      // Nodo de audio.
-      //console.log(contextoDeAudio);
       let analizador = contextoDeAudio.createAnalyser();
       let bitCounter = contextoDeAudio.sampleRate;
       datos_de_fuente.connect(analizador);
@@ -185,10 +92,10 @@ export default class PTuner extends Component {
         if (Hz === notasArray[noteMusic][1]) {
         }
         if (Hz < notasArray[noteMusic][1]) {
-          AumentoLevel = (notasArray[noteMusic][1] - Hz) * 100;
+          AumentoLevel = (notasArray[noteMusic][1] - Hz) * 45;
           DecrementoLevel = 0;
         } else {
-          DecrementoLevel = (Hz - notasArray[noteMusic][1]) * 100;
+          DecrementoLevel = (Hz - notasArray[noteMusic][1]) * 45;
           AumentoLevel = 0;
         }
 
@@ -232,7 +139,7 @@ export default class PTuner extends Component {
         navigator.getUserMedia =
           navigator.getUserMedia ||
           navigator.webkitGetUserMedia ||
-          navigator.msGetUserMedia  ||
+          navigator.msGetUserMedia ||
           navigator.mozGetUserMedia;
         navigator.getUserMedia(medio, llamada, error);
       } catch (e) {
